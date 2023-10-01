cfe5f8fedbfad979bff292e45d53a601fafdbed9
Daniel-k-dev <juyeon10120@naver.com>
2023-10-01 09:35:02 +0900

    학습테스트 실습 요구사항1 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,15 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+
+    @DisplayName("요구사항 1")
+    @Test
+    void split() {
+        String [] result1 = "1,2".split(",");
+        assertThat(result1).containsExactly("1", "2");
+        String [] result2 = "1".split(",");
+        assertThat(result2).containsExactly("1");
     }
 }
