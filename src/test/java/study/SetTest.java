c285a823a8d2834339c18c9e89bb62b7c066de01
Daniel-k-dev <juyeon10120@naver.com>
2023-10-01 10:08:41 +0900

    학습테스트 실습 Set Collection 요구사항2 완료

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,12 +3,15 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static sun.jvm.hotspot.runtime.BasicObjectLock.size;
 
 public class SetTest {
@@ -27,5 +30,12 @@ public class SetTest {
     @Test
     void size () {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("요구사항 2")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void contains (int i) {
+        assertTrue(numbers.contains(i));
     }
 }
