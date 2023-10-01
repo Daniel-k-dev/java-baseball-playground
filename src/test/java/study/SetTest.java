257d960c5ea8b1d3bdb6a1401746564a16a1cd03
Daniel-k-dev <juyeon10120@naver.com>
2023-10-01 09:59:04 +0900

    학습테스트 실습 Set Collection 요구사항1 완료

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,31 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static sun.jvm.hotspot.runtime.BasicObjectLock.size;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("요구사항 1")
+    @Test
+    void size () {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
